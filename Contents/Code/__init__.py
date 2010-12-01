c9058bce56b51af39c6f699e8c0ebf6c07c00eb0
Pierre <p.dellanave@gmail.com>
2010-11-30 23:42:06 -0800

    Fixed duration parsing and Xpath namespace handling - Fixed HTTP request in Update cache to be v2 compliant

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,7 +1,7 @@
 
 DN_PREFIX    = '/video/democracynow'
 DN_RSS_FEED  = 'http://www.democracynow.org/podcast-video.xml'
-DN_NAMESPACE = {'i':'http://www.itunes.com/dtds/podcast-1.0.dtd'}
+DN_NAMESPACE = {'itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd'}
 CACHE_INTERVAL = 3600
 
 ####################################################################################################
@@ -14,17 +14,20 @@ def Start():
   
 ####################################################################################################
 def UpdateCache():
-  HTTP.Request(DN_RSS_FEED)
+  HTTP.Request(DN_RSS_FEED).content
 
 ####################################################################################################
 def Videos():
   dir = MediaContainer()
-  for v in XML.ElementFromURL(DN_RSS_FEED, cacheTime=CACHE_INTERVAL).xpath("//item"):
+  for v in XML.ElementFromURL(DN_RSS_FEED, cacheTime=CACHE_INTERVAL).xpath("//item", namespaces=DN_NAMESPACE):
     title = v.find('title').text
     description = v.find('description').text
     date = Datetime.ParseDate(v.find('pubDate').text).strftime('%a %b %d, %Y')
     url = v.find('enclosure').get('url')
-    duration = v.xpath('i:duration', namespaces=DN_NAMESPACE)[0].text.split(':')
-    duration = (int(duration[0])*60 + int(duration[1])) * 1000
+    try:
+      duration = v.xpath('itunes:duration', namespaces=DN_NAMESPACE)[0].text.split(':')
+      duration = (int(duration[0])*60 + int(duration[1])) * 1000
+    except:
+      duration = 0
     dir.Append(VideoItem(url, date, subtitle='Democracy Now!', duration=duration, summary=description, thumb=R('icon-default.png')))
   return dir
